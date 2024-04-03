64feefe8603ce16d390ef5fe218b1ad939e8dee5
isarbela <panzeref@gmail.com>
2024-04-03 10:13:31 -0300

    Implemented Candidate Repository using SQL

---
+++ election-management/src/test/java/domain/CandidateRepositoryTest.java
@@ -0,0 +1,52 @@
+package domain;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.*;
+
+public abstract class CandidateRepositoryTest {
+
+    public abstract CandidateRepository repository();
+
+    @Test
+    void save() {
+        Candidate candidate = Instancio.create(Candidate.class);
+        repository().save(candidate);
+
+        Optional<Candidate> result = repository().findById(candidate.id());
+
+        assertTrue(result.isPresent());
+        assertEquals(candidate, result.get());
+    }
+
+    @Test
+    void findAll() {
+        var candidates = Instancio.stream(Candidate.class).limit(10).toList();
+
+        repository().save(candidates);
+
+        List<Candidate> result = repository().findAll();
+
+        assertEquals(candidates.size(), result.size());
+    }
+
+    @Test
+    void findByName() {
+        var candidate1 = Instancio.create(Candidate.class);
+        var candidate2 = Instancio.of(Candidate.class).set(field("familyName"), "Magalhães").create();
+        var query = new CandidateQuery.Builder().name("MAG").build();
+
+        repository().save(List.of(candidate1, candidate2));
+
+        List<Candidate> result = repository().find(query);
+
+        assertEquals(1, result.size());
+        assertEquals(candidate2, result.get(0));
+    }
+
+}
